229802bc68294951eef27942fd5f675195107fa6
chsevero <chsevero@outlook.com.br>
2021-12-21 08:23:20 -0300

    enable xml format to endpoint authors.

--- api/app/routers/author.py
+++ api/app/routers/author.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter
 from app.schemas.author import Author as SchemaAuthor
 from app.services import author as author_service
@@ -8,3 +9,8 @@ router = APIRouter()
 @router.post("/add-author/", tags=["Author"], response_model=SchemaAuthor)
 def add_author(author: SchemaAuthor):
     return author_service.add_author(author)
+
+
+@router.get("/author/", tags=["Author"])
+def get_authors(format: Optional[str] = None):
+    return author_service.authors(format)
--- api/app/services/author.py
+++ api/app/services/author.py
@@ -1,6 +1,9 @@
 from app.models.author import Author as ModelAuthor
 from app.schemas.author import Author as SchemaAuthor
 from fastapi_sqlalchemy import db
+from fastapi.encoders import jsonable_encoder
+from xml.dom.minidom import parseString
+from dicttoxml import dicttoxml
 
 
 def add_author(author: SchemaAuthor):
@@ -8,3 +11,12 @@ def add_author(author: SchemaAuthor):
     db.session.add(db_author)
     db.session.commit()
     return db_author
+
+
+def authors(format):
+    authors = db.session.query(ModelAuthor).all()
+    if format == "xml":
+        authors_json = jsonable_encoder(authors)
+        authors_xml = dicttoxml(authors_json)
+        return authors_xml
+    return authors
