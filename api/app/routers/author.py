03e8a421d6e0957fe4b95ae34c1cca53bd84c8de
chsevero <chsevero@outlook.com.br>
2021-12-21 20:40:15 -0300

    Added endpoints update, delete, get_by_id to author model

--- api/app/routers/author.py
+++ api/app/routers/author.py
@@ -11,6 +11,21 @@ def add_author(author: SchemaAuthor):
     return author_service.add_author(author)
 
 
-@router.get("/author/", tags=["Author"])
+@router.get("/authors/", tags=["Author"])
 def get_authors(format: Optional[str] = None):
     return author_service.authors(format)
+
+
+@router.get("/author/{id}", tags=["Author"])
+def get_author_by_id(id: int, format: Optional[str] = None):
+    return author_service.author(id, format)
+
+
+@router.put("/author/{id}", tags=["Author"], response_model=SchemaAuthor)
+def update_author(id: int, author: SchemaAuthor, format: Optional[str] = None):
+    return author_service.update_author(id, author, format)
+
+
+@router.delete("/author/{id}", tags=["Author"])
+def delete_author(id: int):
+    return author_service.delete_author(id)
--- api/app/services/author.py
+++ api/app/services/author.py
@@ -20,3 +20,45 @@ def authors(format):
         authors_xml = dicttoxml(authors_json)
         return authors_xml
     return authors
+
+
+def author(id, format):
+
+    author_ = db.session.get(ModelAuthor, {"id": id})
+    if author_ is not None:
+        if format == "xml":
+            author_json = jsonable_encoder(author_)
+            return dicttoxml(author_json)
+        return author_
+
+    message = {"message": f"Author with id: {id} does not exists!"}
+    if format == "xml":
+        message_json = jsonable_encoder(message)
+        return dicttoxml(message_json)
+    return message
+
+
+def update_author(id, author, format):
+    author_ = db.session.get(ModelAuthor, {"id": id})
+    if author_ is not None:
+        author_.name = author.name
+        db.session.commit()
+        if format == "xml":
+            author_json = jsonable_encoder(author_)
+            return dicttoxml(author_json)
+        return author_
+
+    message = {"message": f"Author with id: {id} does not exists!"}
+    if format == "xml":
+        message_json = jsonable_encoder(message)
+        return dicttoxml(message_json)
+    return message
+
+
+def delete_author(id):
+    author_ = db.session.get(ModelAuthor, {"id": id})
+    if author_ is not None:
+        db.session.delete(author_)
+        db.session.commit()
+        return author_
+    return {"message": f"Author with id: {id} does not exists!"}
