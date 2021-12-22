ed3d2e60d5a2271b0c62a4891ad29409832c50b4
chsevero <chsevero@outlook.com.br>
2021-12-21 23:43:52 -0300

    user model creation

--- api/app/main.py
+++ api/app/main.py
@@ -1,15 +1,18 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from fastapi_sqlalchemy import DBSessionMiddleware
 from .routers.author import router as author_router
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 app.include_router(author_router)
 
 
 @app.get("/", tags=["Book-API"])
-def root():
+def root(token: str = Depends(oauth2_scheme)):
     return {"message": "Book-API alive!"}
